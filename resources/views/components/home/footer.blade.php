0dfff789b3a74acd1028e009a01926bc43643d8b
root <root@localhost.localdomain>
2022-11-12 20:03:29 -0500

    FOOTER::text edit and href edit

--- resources/views/components/home/footer.blade.php
+++ resources/views/components/home/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-lg-3 col-md-12">
                 <p>
-                    <a href="index.html" title="Findoctor">
+                    <a href="/" title="">
                         <img src="../img/logo.png" alt="" width="163" height="36" class="img-fluid">
                     </a>
                 </p>
@@ -11,36 +11,36 @@
             <div class="col-lg-3 col-md-4">
                 <h5>About</h5>
                 <ul class="links">
-                    <li><a href="#0">About us</a></li>
-                    <li><a href="blog.html">Blog</a></li>
-                    <li><a href="#0">FAQ</a></li>
-                    <li><a href="login.html">Login</a></li>
-                    <li><a href="register.html">Register</a></li>
+                    <li><a href="/404">About us</a></li>
+                    <li><a href="/404">Blog</a></li>
+                    <li><a href="/404">FAQ</a></li>
+                    <li><a href="/patient/register">Login</a></li>
+                    <li><a href="/patient/register">Register</a></li>
                 </ul>
             </div>
             <div class="col-lg-3 col-md-4">
                 <h5>Useful links</h5>
                 <ul class="links">
-                    <li><a href="#0">Doctors</a></li>
-                    <li><a href="#0">Clinics</a></li>
-                    <li><a href="#0">Specialization</a></li>
-                    <li><a href="#0">Join as a Doctor</a></li>
-                    <li><a href="#0">Download App</a></li>
+                    <li><a href="/doctors">Doctors</a></li>
+                    <li><a href="/404">Clinics</a></li>
+                    <li><a href="/404">Specialization</a></li>
+                    <li><a href="/doctor/register">Join as a Doctor</a></li>
+                    <li><a href="/404">Download App</a></li>
                 </ul>
             </div>
             <div class="col-lg-3 col-md-4">
                 <h5>Contact with Us</h5>
                 <ul class="contacts">
-                    <li><a href="tel://61280932400"><i class="icon_mobile"></i> + 61 23 8093 3400</a></li>
-                    <li><a href="mailto:info@findoctor.com"><i class="icon_mail_alt"></i> help@findoctor.com</a></li>
+                    <li><a href="tel://905057748530"><i class="icon_mobile"></i> + 90 505 774 8530</a></li>
+                    <li><a href="mailto:info@findoctor.com"><i class="icon_mail_alt"></i> oguzcanturan30@gmail.com</a></li>
                 </ul>
                 <div class="follow_us">
                     <h5>Follow us</h5>
                     <ul>
-                        <li><a href="#0"><i class="social_facebook"></i></a></li>
-                        <li><a href="#0"><i class="social_twitter"></i></a></li>
-                        <li><a href="#0"><i class="social_linkedin"></i></a></li>
-                        <li><a href="#0"><i class="social_instagram"></i></a></li>
+                        <li><a href="https://www.facebook.com/DoktoraDanis"><i class="social_facebook"></i></a></li>
+                        <li><a href="https://twitter.com/DoktoraDanis"><i class="social_twitter"></i></a></li>
+                        <li><a href="https://www.facebook.com/DoktoraDanis"><i class="social_linkedin"></i></a></li>
+                        <li><a href="https://www.instagram.com/doktora_danis/"><i class="social_instagram"></i></a></li>
                     </ul>
                 </div>
             </div>
@@ -50,12 +50,12 @@
         <div class="row">
             <div class="col-md-8">
                 <ul id="additional_links">
-                    <li><a href="#0">Terms and conditions</a></li>
-                    <li><a href="#0">Privacy</a></li>
+                    <li><a href="/404">Terms and conditions</a></li>
+                    <li><a href="/404">Privacy</a></li>
                 </ul>
             </div>
             <div class="col-md-4">
-                <div id="copy">© 2021 Findoctor</div>
+                <div id="copy">© 2022 Doktora Danış</div>
             </div>
         </div>
     </div>
