48bad1f4ea5416d9b54bab10de17df9619b1edc8
root <root@localhost.localdomain>
2022-11-12 19:56:59 -0500

    DOCTORSLIDER::edit Height fix and bg fix

--- resources/views/livewire/doctor-slider.blade.php
+++ resources/views/livewire/doctor-slider.blade.php
@@ -6,13 +6,13 @@
     </div>
     <div id="reccomended" class="owl-carousel owl-theme">
       @foreach ($doctors as $doctor)
-        <div class="item" style="box-shadow: rgb(0 0 0 / 16%) 0px 1px 4px; border: 2px solid #ddd; border-radius: 5px;">
+        <div class="item" style="height: 400px; box-shadow: rgb(0 0 0 / 16%) 0px 1px 4px; border: 2px solid #ddd; border-radius: 5px;">
           <a href="detail-page.html">
             <div class="views"><i class="icon-eye-7"></i>140</div>
             <div class="title">
               <h4>{{$doctor->getFullName()}}</h4>
               <p>{{$doctor->field()->name}}</p>
-            </div><img src="{{URL::asset('images/doctors/profile/'.$doctor->profile_picture)}}" style="width: 350px; height: 500px; object-fit: contain; padding-right: 37px;">
+            </div><img src="{{URL::asset('images/doctors/profile/'.$doctor->profile_picture)}}" style="width: 350px; height: 400px; object-fit: contain; padding-right: 37px;">
           </a>
         </div>
       @endforeach
