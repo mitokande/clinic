9c7a4b36a3db5feb925db6ed9b68a9f9612ab829
root <root@localhost.localdomain>
2022-11-12 21:15:34 -0500

    USERREVIEW::add model -mcfs --r and migrate

---
+++ database/migrations/2022_11_13_021247_create_user_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('content');
+            $table->tinyInteger('star');
+            $table->string('picture');
+            $table->string('user_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_reviews');
+    }
+};
